115c80cb423177afd5ee2600a144e38072de7e5c
Damián morales <60529414+dam788@users.noreply.github.com>
2021-04-17 22:23:42 -0300

    esqueleto de todolist

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+//!reducers
+import { todoReducer } from "./todo/todoReducer";
+
+//! combina todos los reducers
+export const reducer = combineReducers({
+  todo: todoReducer,
+});
---
+++ src/redux/store.js
@@ -0,0 +1,26 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { reducer } from "./rootReducer";
+
+//! agrega middlewares a nuestras acciones
+const middleware = [];
+
+// !configuracion de persistor
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whiteList: ["todosList"],
+};
+
+const rootReducer = persistReducer(persistConfig, reducer);
+
+//! creamos el store y le agregamos el compose para usar en el naveagdor el react redux
+export const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export const persistor = persistStore(store);
